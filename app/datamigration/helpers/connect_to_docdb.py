8a2bd812c0cae56fa69b6cb9669903cc82fcd3ac
yumeng <yumeng@yumengs-MacBook-Air.local>
2025-03-25 21:59:46 -0700

    Lambda helpers to retrieve data from DocDB

---
+++ app/datamigration/helpers/connect_to_docdb.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+import ssl
+
+def connect_to_docdb(credentials):   
+    # Establish the connection to DocumentDB
+    if not credentials:
+        print("No credentials provided")
+        return None
+        
+    try:
+        client = MongoClient("mongodb://docdbadmin:Apply2025!@docdbclusterinstance15e47fa-0etykuhor6oj.ck1k80ayiam1.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=%2FUsers%2Fyumeng%2FDesktop%2FPersonalProjects%2Fglobal-bundle.pem&retryWrites=false&readPreference=nearest", ssl=True)
+        client.admin.command('ping')
+        print("Connected to DocumentDB!")
+
+        # Sample operation: List databases
+        databases = client.list_database_names()
+        print("Available Databases:", databases)
+        
+        db = client['DBMigrationNShardingRaw']
+        collection = db['ECommerceRawData']
+        
+        documents = collection.find({"toBeMigrated": True})
+        for document in documents:
+            print("Document to be migrated:", document)
+        
+        return client
+
+    except Exception as e:
+        print("An error occurred while connecting to DocumentDB:", e)
+        return None
--- app/datamigration/main.py
+++ app/datamigration/main.py
@@ -1,2 +1,33 @@
-def handler(event, context):
-    return {"statusCode": 200, "body": "Hello from Data Migration Lambda!"}
+# def handler(event, context):
+#     return {"statusCode": 200, "body": "Hello from Data Migration Lambda!"}
+
+from helpers.connect_to_docdb import connect_to_docdb
+from helpers.get_docdb_credentials import get_docdb_credentials
+import sys
+import os
+
+# Add the app directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def main():
+	try:
+		credentials = get_docdb_credentials()
+		if not credentials:
+			print("Failed to retrieve credentials")
+			return
+
+		client = connect_to_docdb(credentials)
+
+		if not client:
+			print("Failed to connect to DocumentDB")
+			return
+
+	except Exception as e:
+		print("Application error:", e)
+	finally:
+		if 'client' in locals() and client:
+			client.close()
+			print("Connection closed")
+
+if __name__ == "__main__":
+    main()
